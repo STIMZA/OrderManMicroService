30a09eb8e2ce7c81276f7da68b0b2fd06ece7982
Matimu Ngoveni <matimu.romeo@outlook.com>
2026-05-28 06:34:26 +0200

    first commit

---
+++ src/main/java/com/assesment/ordermanmicroservice/OrderManMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assesment.ordermanmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assesment/ordermanmicroservice/OrderManMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assesment.ordermanmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
